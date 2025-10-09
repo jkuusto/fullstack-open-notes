0c1be722d256644b669b8f573f2b3f5136f67581
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-10-09 15:40:32 +0300

    Configure lint, fix issues

--- models/note.js
+++ models/note.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI;
 console.log("connectiong to", url);
 mongoose
   .connect(url)
-  .then((result) => {
+  .then(() => {
     console.log("connected to MongoDB");
   })
   .catch((error) => {
