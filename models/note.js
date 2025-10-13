1b1fdd6144a836076de18c186bf19e128642e497
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-10-13 12:29:33 +0300

    Part4: Refactor

--- models/note.js
+++ models/note.js
@@ -1,19 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("connectiong to", url);
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
